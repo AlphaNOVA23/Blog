1dd7aa124b0698a8c0e1bbb0e1d3fa39db4950bd
Tanmay Shinde <tanmaysm2005@gmail.com>
2024-09-17 21:37:53 +0530

    igonring github_essentials.txt

--- blog.py
+++ blog.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "my super secret key"
 
 @app.route('/')
 def index():
@@ -24,6 +28,20 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template("500.html"), 500
 
+#Create a Form class
+class NamerForm(FlaskForm):
+    name = StringField("What's Your Name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+    
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    #validate form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template("name.html", name=name, form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
